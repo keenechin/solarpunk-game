bc94d99ee4e9e7d8d24995d72ec2e2f613ebbbaa
Keene Chin <keene.chin@gmail.com>
2022-01-17 22:44:33 -0500

    updated base rendering of screen, set game to 60 fps

--- sunfield.py
+++ sunfield.py
@@ -24,14 +24,19 @@ def handle_events(scene):
             scene.motion_handler(event)
         
 
-def main(width=1080, height=720):
+def main(width=600, height=600):
     pg.init()
+    clock = pg.time.Clock()
     screen = pg.display.set_mode((width, height))
-    scene = Overworld()
+    scene = Overworld(screen)
+    fps = 60
     while True:
         pg.display.update()
         handle_events(scene)
-        screen.fill((100,50,0))
+        screen.fill(pg.Color(255,255,255))
+        scene.update()
+        scene.render()
+        clock.tick(fps)
 
 if __name__ == "__main__":
     main()
